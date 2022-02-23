ec9da587be917d8aa9699bdffd227e57610a0042
m-milkin <m-milkin@yandex-team.ru>
2022-02-23 16:10:43 +0300

    added is_integral, also added few algorithm functions, like distance and swap

--- srcs/algorithm.hpp
+++ srcs/algorithm.hpp
@@ -3,8 +3,9 @@
 #include "iterators.hpp"
 #include "iterators_traits.hpp"
 
-
 namespace ft {
+
+    /* lexicographical_compare */
     template< class InputIt1, class InputIt2 >
     bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
                                 InputIt2 first2, InputIt2 last2 ) {
@@ -34,6 +35,7 @@ namespace ft {
         return (size1 >= size2);
     }
 
+    /* equal */
     template< class InputIt1, class InputIt2 >
     bool equal( InputIt1 first1, InputIt1 last1,
                 InputIt2 first2, InputIt2 last2) {
@@ -49,5 +51,47 @@ namespace ft {
             return false;
         }
     }
+
+    /* algorithm */
+    template<class InputIt>
+    typename iterator_traits<InputIt>::difference_type distance(InputIt first, InputIt last) {
+        typename iterator_traits<InputIt>::difference_type diff;
+        while (first != last) {
+            ++diff; ++first;
+        }
+        return diff;
+    }
+
+    template<class T>
+    void swap(T& a, T& b) {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+
+    /* ft::is_integral */
+    template<class T, bool v>
+    struct integral_constant {
+        static const bool value = v;
+        typedef T value_type;
+        typedef integral_constant<T, v> type;
+    };
+
+    template<class T> struct is_integral : public integral_constant<T, false> {};
+    template<> struct is_integral<bool> : public integral_constant<bool, true> {};
+    template<> struct is_integral<char> : public integral_constant<char, true> {};
+    template<> struct is_integral<char16_t> : public integral_constant<char16_t, true> {};
+    template<> struct is_integral<char32_t> : public integral_constant<char32_t, true> {};
+    template<> struct is_integral<wchar_t> : public integral_constant<wchar_t, true> {};
+    template<> struct is_integral<short> : public integral_constant<short, true> {};
+    template<> struct is_integral<int> : public integral_constant<int, true> {};
+    template<> struct is_integral<long> : public integral_constant<long, true> {};
+    template<> struct is_integral<long long> : public integral_constant<long long, true> {};
+    template<> struct is_integral<unsigned short> : public integral_constant<unsigned short, true> {};
+    template<> struct is_integral<unsigned int> : public integral_constant<unsigned int, true> {};
+    template<> struct is_integral<unsigned long> : public integral_constant<unsigned long, true> {};
+    template<> struct is_integral<unsigned long long> : public integral_constant<unsigned long long, true> {};
+
+
 
 } //namespace ft
