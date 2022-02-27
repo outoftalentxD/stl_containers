baab1fe90807bde079a9bfc6d40c803565f5f8b6
m-milkin <m-milkin@yandex-team.ru>
2022-02-27 20:01:33 +0300

    cannot not solve node-parent problem, so i decided to handle simple btree. Binary tree approach will be commited on next iteration

--- srcs/map.hpp
+++ srcs/map.hpp
@@ -23,17 +23,101 @@ namespace ft {
         typedef typename allocator_type::const_pointer const_pointer;
 
     private:
-        typedef Treap<value_type, key_compare, allocator_type> tree_type;
+        class pair_compare {
+        private:
+            key_compare _cmp;
+        public:
+            pair_compare(const key_compare& cmp) : _cmp(cmp) {
+
+            }
+
+            bool operator()(const value_type& lhs, const value_type& rhs) const {
+                return (_cmp(lhs.first, rhs.first));
+            }
+        };
+
+    public:
+        typedef pair_compare value_compare;
+
+    private:
+        typedef Treap<value_type, value_compare, allocator_type> tree_type;
         typedef typename tree_type::node_type node_type;
 
     public:
         typedef typename tree_type::iterator iterator;
         typedef typename tree_type::const_iterator const_iterator;
-        typedef typename tree_type::reverse_terator reverse_iterator;
-        typedef typename tree_type::const_reverse_iterator const_reverse_iterator;
+        // typedef typename tree_type::reverse_terator reverse_iterator;
+        // typedef typename tree_type::const_reverse_iterator const_reverse_iterator;
 
     public:
-        
+        explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) : _allocator(alloc), _cmp(comp),
+                                                                                                                    _treap(tree_type(comp, alloc)) {
+
+        }
+
+        map(const map& other) : _allocator(other._allocator), _cmp(other._cmp), _treap(other._treap) {
+
+        }
+
+        map& operator=(const map& other) {
+            if (this != &other) {
+                _allocator = other._allocator;
+                _cmp = other._cmp;
+                _treap = other._treap;
+            }
+            return *this;
+        }
+
+        allocator_type get_allocator() const {
+            return _allocator;
+        }
+
+        ~map() {
+
+        }
+
+    /* Element access */
+    public:
+        void at(const key_type& key) {
+            _treap.find(ft::make_pair(key, mapped_type()));
+            // iterator it = _treap.find(ft::make_pair(value.first, value.second));
+        }
+
+    /* Iterators */
+    public:
+        iterator begin() {
+            return _treap.begin();
+        }
+
+        const_iterator begin() const {
+            return _treap.begin();
+        }
+
+        iterator end() {
+            return _treap.end();
+        }
+
+        const_iterator end() const {
+            return _treap.end();
+        }
+
+
+
+    /* Modifiers */
+    public:
+        void insert(const value_type& value) {
+            _treap.push(value);
+        }
+
+    /* Element access */
+    public:
+    /* Element access */
+    public:
+    /* Element access */
+    public:
+    /* Element access */
+    public:
+
     private:
         allocator_type _allocator;
         tree_type _treap;
--- srcs/treap.hpp
+++ srcs/treap.hpp
@@ -82,11 +82,11 @@ namespace ft {
         }
 
         reference operator*() const {
-            return *(_pnode->value);
+            return _pnode->value;
         }
 
         pointer operator->() const {
-            return _pnode->value;
+            return &(_pnode->value);
         }
 
         TreapIter& operator++() {
@@ -103,8 +103,9 @@ namespace ft {
             return *this;
         }
 
-        TreapIter& operator++(int) {
+        TreapIter operator++(int) {
             TreapIter<value_type> temp = *this;
+            std::cout << "here\n";
             if (_pnode->right) {
                 _pnode = _treap_subtree_min(_pnode->right);
             } else {
@@ -115,7 +116,7 @@ namespace ft {
                 }
                 _pnode = _pnode->parent;
             }
-            return *this;return temp;
+            return temp;
         }
 
         TreapIter& operator--() {
@@ -132,7 +133,7 @@ namespace ft {
             return *this;
         }
 
-        TreapIter& operator--(int) {
+        TreapIter operator--(int) {
             TreapIter<value_type> temp = *this;
             if (_pnode->left) {
                 _pnode = _treap_subtree_max(_pnode->left);
@@ -179,13 +180,13 @@ namespace ft {
     }
 
     /* Treap */
-    template<class T, class Compare = ft::less<T>, class Alloc = std::allocator<T> >
+    template<class Value, class Compare = ft::less<Value>, class Alloc = std::allocator<Value> >
     class Treap {
     public:
-        typedef T value_type;
+        typedef Value value_type;
         typedef Alloc allocator_type;
         typedef Compare compare_type;
-        typedef _node<T> node_type;
+        typedef _node<Value> node_type;
 
         typedef typename allocator_type::template rebind<node_type>::other node_allocator;
 
@@ -201,19 +202,19 @@ namespace ft {
         typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 
     public:
-
-
-        Treap(compare_type& cmp, const allocator_type& allocator = Alloc()) : _cmp(cmp), _allocator(allocator), _node_allocator(node_allocator()), _size(0) {
-            _root = _node_allocator.allocate(1);
-            _node_allocator.construct(_root, value_type());
+        Treap(const compare_type& cmp, const allocator_type& allocator = Alloc()) : _cmp(cmp), _allocator(allocator), _node_allocator(node_allocator()), _size(0) {
+            _header = _node_allocator.allocate(1);
+            _node_allocator.construct(_header, value_type());
+            _root = nullptr;
         }
 
-        Treap(const Treap& other) : _cmp(other._cmp), _allocator(other._allocator), _node_allocator(other._node_allocator), _root(other._root), _size(other._size) {
+        Treap(const Treap& other) : _cmp(other._cmp), _allocator(other._allocator), _node_allocator(other._node_allocator), _root(other._root), _header(other._header), _size(other._size) {
 
         }
 
         Treap& operator=(const Treap& other) {
             if (this != &other) {
+                _header = other._header;
                 _root = other._root;
                 _allocator = other._allocator;
                 _cmp = other._cmp;
@@ -224,15 +225,107 @@ namespace ft {
         }
 
         ~Treap() {
-            _delete_treap();
+            _delete_treap(_root);
         }
 
     public:
         void push(const value_type& value) {
-            _root._insert(_root, new ft::_node<value_type>(value));
+            node_pointer pnode = _search(_root, value);
+            if (pnode) {
+                pnode->value.second = value.second;
+            } else {
+                if (_root) {
+                    _root->parent = nullptr;
+                }
+                _insert(_root, _create_node(value));
+                _assign_header(_root);
+            }
         }
 
+    /* iterators */
     public:
+        iterator begin() {
+            return iterator(_subtree_min(_root));
+        }
+
+        const_iterator begin() const {
+            return const_iterator(_subtree_min(_root));
+        }
+
+        iterator end() {
+            return iterator(_header);
+        }
+
+        const_iterator end() const {
+            return const_iterator(_header);
+        }
+
+
+    public:
+        void print(const node_pointer treap) const {
+            if (treap) {
+                print(treap->left);
+                std::cout << treap->value.first << std::endl;
+                print(treap->right);
+            }
+        }
+        void find(const value_type& value) const {
+            print(_root);
+        }
+
+    private:
+        node_pointer _subtree_min(node_pointer treap) const {
+            while (treap->left) {
+                treap = treap->left;
+            }
+            return treap;
+        }
+
+        node_pointer _subtree_max(node_pointer treap) const {
+            while (treap->right) {
+                treap = treap->right;
+            }
+            return treap;
+        }
+
+        node_pointer _create_node(const value_type& value) {
+            node_pointer pnode = _node_allocator.allocate(1);
+            _node_allocator.construct(pnode, value);
+            return pnode;
+        }
+
+        node_pointer _search(node_pointer treap, const value_type& value) const {
+            if (!treap) {
+                return nullptr;
+            } else {
+                if (_cmp(value, treap->value)) {
+                    return _search(treap->left, value);
+                } else if (_cmp(treap->value, value)) {
+                    return _search(treap->right, value);
+                } else {
+                    return treap;
+                }
+            }
+        }
+
+        void _insert(node_pointer& treap, node_pointer node) {
+            if (!treap) {
+                treap = node;
+                _header->left = treap; _header->right = treap;
+                treap->parent = _header;
+            } else if (node->weight > treap->weight) {
+                _split(treap, node->value, node->left, node->right);
+                treap = node;
+                _assign_parents(treap, treap->left, treap->right);
+            } else {
+                if (_cmp(node->value, treap->value)) {
+                    _insert(treap->left, node);
+                } else {
+                    _insert(treap->right, node);
+                }
+            }
+        }
+
         void _split(node_pointer treap, value_type& value, node_pointer& left, node_pointer& right) {
             if (!treap) {
                 left = right = nullptr;
@@ -241,6 +334,7 @@ namespace ft {
                 right = treap;
             } else {
                 _split(treap->right, value, treap->right, right);
+                left = treap;
             }
         }
 
@@ -256,24 +350,6 @@ namespace ft {
             }
         }
 
-        void _insert(node_pointer& treap, node_pointer node) {
-            if (!treap) {
-                treap = node;
-            } else if (node->weight > treap->weight) {
-                _split(treap, node->value, node->left, node->right);
-                treap = node;
-            } else {
-                if (_cmp(node->value, treap->value)) {
-                    _insert(treap->left, node);
-                } else if (_cmp(treap->value, node->value)) {
-                    _insert(treap->right, node);
-                } else { // if is equal, test later
-                    treap->value = node->value;
-                    return ;
-                }
-            }
-        }
-
         void _delete_treap(node_pointer root) {
             if (root) {
                 _delete_treap(root->left);
@@ -282,12 +358,26 @@ namespace ft {
                 _node_allocator.deallocate(root, 1);
             }
         }
+        void _assign_parents(node_pointer treap, node_pointer left, node_pointer right) {
+            if (left) {
+                left->parent = treap;
+            }
+            if (right) {
+                right->parent = treap;
+            }
+        }
+
+        void _assign_header(node_pointer _root) {
+            _root->parent = _header;
+            _header->left = _header->right = _root;
+        }
 
     private:
         allocator_type _allocator;
         node_allocator _node_allocator;
         compare_type _cmp;
         node_pointer _root;
+        node_pointer _header;
         size_type _size;
     };
 } //namespace ft;
